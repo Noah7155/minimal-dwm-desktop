90fc66e09e0d1d87f1dfd792e72578ba7b86dc61
Rubber Ducky | Destroyer of Worlds <74378703+Dragonflame7155@users.noreply.github.com>
2022-05-02 17:41:51 -0400

    setup script

---
+++ archsetup.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+echo "generating directories and files..."
+
+mkdir --parents ~/.config/kitty
+mkdir --parents ~/.config/picom
+touch ~/.zshrc
+touch ~/.xinitrc
+
+echo "gathering packages..."
+
+sudo pacman -Syu zsh xorg xorg-xinit neofetch picom dmenu kitty nitrogen emacs dwm
+
+echo "collecting dwm-6.3.tar.gz..."
+
+wget https://dl.suckless.org/dwm/dwm-6.3.tar.gz
+
+echo "gathering github repositories..."
+
+echo "cloning minimal-dwm-desktop..."
+
+git clone https://github.com/Dragonflame7155/minimal-dwm-desktop.git
+
+echo "cloning shell-files..."
+
+git clone https://github.com/Dragonflame7155/shell-files.git
+
+echo "cloning config-files-dump..."
+
+git clone https://github.com/Dragonflame7155/config-files-dump.git
+
+echo "cloning emacs-files..."
+
+git clone https://github.com/Dragonflame7155/emacs-files.git
+
+echo "unpacking dwm-6.3..."
+
+tar -xvf dwm-6.3.tar.gz
+
+cd dwm-6.3
+
+mv ~/minimal-dwm-desktop/config.h .
+
+cd ..
+
+mv ~/config-files-dump/kitty.conf ~/.config/kitty/
+
+cp ~/minimal-dwm-desktop/xinitrc ~/.xinitrc
+
+cd dwm-6.3
+
+sudo make install
+
+startx
